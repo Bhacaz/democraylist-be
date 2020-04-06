9d552b84a53328804a1d4dbd6c59f006ed6afac9
Jean-Francis Bastien <bhacaz@gmail.com>
2020-04-05 21:26:23 -0400

    Change RSpotify credential to initialzier

--- config/application.rb
+++ config/application.rb
@@ -31,7 +31,6 @@ module Democraylist
 
     # Don't generate system test files.
     config.generators.system_tests = nil
-    # RSpotify::authenticate(ENV['spotify_client_id'], ENV['spotify_client_secret'])
 
     config.middleware.insert_before 0, Rack::Cors do
       allow do
--- config/initializers/rspotify.rb
+++ config/initializers/rspotify.rb
@@ -1,2 +1,4 @@
 
+RSpotify::authenticate(ENV['spotify_client_id'], ENV['spotify_client_secret'])
+
 require_relative '../../app/lib/r_spotify/user_extension'
