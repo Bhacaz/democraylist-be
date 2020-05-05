c977e14e04e13c090c586b529fa7bf592521ea93
Jean-Francis Bastien <bhacaz@gmail.com>
2020-05-05 08:16:33 -0400

    No retry on send notif

--- app/jobs/send_notif_job.rb
+++ app/jobs/send_notif_job.rb
@@ -1,10 +1,9 @@
 
-class SendNotifJob < ApplicationJob
-  queue_as :default
-  retry_on Exception, attempts: 3
+class SendNotifJob
+  include Sidekiq::Worker
+  sidekiq_options retry: false
 
   def perform(track_id)
     NotificationService.broadcast_added_track(Track.find(track_id))
   end
 end
-
