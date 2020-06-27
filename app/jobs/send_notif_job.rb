6acdcd1512d681944dac16437838992c6b888bb7
Jean-Francis Bastien <bhacaz@gmail.com>
2020-06-27 19:52:45 -0400

    Dont delete push preference on failed

--- app/jobs/send_notif_job.rb
+++ app/jobs/send_notif_job.rb
@@ -4,7 +4,5 @@ class SendNotifJob < ApplicationJob
   def perform(notif_data, user_id)
     push_notif_preference = User.find(user_id).push_notif_preference
     NotificationService.send_push notif_data, push_notif_preference.preference
-  rescue Webpush::ExpiredSubscription => _e
-    push_notif_preference.destroy
   end
 end
