7ee6f78220270a2b386e2960e2cf64ac7cb10e91
Jean-Francis Bastien <bhacaz@gmail.com>
2020-09-04 16:10:11 -0400

    Update rspotify

--- config/initializers/rspotify.rb
+++ config/initializers/rspotify.rb
@@ -1,5 +1,4 @@
 
 RSpotify::authenticate(ENV['spotify_client_id'], ENV['spotify_client_secret'])
 
-require_relative '../../app/lib/r_spotify/user_extension'
 require_relative '../../app/lib/r_spotify/track_extension'
