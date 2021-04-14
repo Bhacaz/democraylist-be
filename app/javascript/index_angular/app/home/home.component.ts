b3c8d58c036b3c752324b4a6dc270ad2ac0d4732
Jean-Francis Bastien <bhacaz@gmail.com>
2021-04-13 20:40:48 -0400

    scss loader works

--- app/javascript/index_angular/app/home/home.component.ts
+++ app/javascript/index_angular/app/home/home.component.ts
@@ -5,11 +5,12 @@ import {DemocraylistService} from '../democraylist/democraylist.service';
 import {environment} from '../../environment';
 import {LocalstorageService} from '../common/localstorage.service';
 import templateString from './home.component.html'
+import styleString from './home.component.scss'
 
 @Component({
   selector: 'app-home',
   template: templateString,
-  // styles: ['./home.component.scss']
+  styles: [styleString]
 })
 export class HomeComponent implements OnInit {
 
--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,7 +1,9 @@
 const { environment } = require('@rails/webpacker')
 const typescript =  require('./loaders/typescript')
 const html =  require('./loaders/html')
+const sass =  require('./loaders/sass')
 
 environment.loaders.prepend('typescript', typescript)
 environment.loaders.prepend('html', html)
+environment.loaders.insert('sass', sass)
 module.exports = environment
