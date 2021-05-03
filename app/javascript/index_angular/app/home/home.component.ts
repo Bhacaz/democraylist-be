9fd3bc4b904877281a51cc259304a3d8c46d46c9
Jean-Francis Bastien <bhacaz@gmail.com>
2021-05-03 07:54:37 -0400

    WIP

--- app/javascript/index_angular/app/home/home.component.ts
+++ app/javascript/index_angular/app/home/home.component.ts
@@ -66,4 +66,5 @@ export class HomeComponent implements OnInit {
       this.isLoading = false
     })
   }
+
 }
--- app/javascript/index_angular/app/playlists/playlist-cover/playlist-cover.component.ts
+++ app/javascript/index_angular/app/playlists/playlist-cover/playlist-cover.component.ts
@@ -3,6 +3,7 @@ import {DemocraylistService} from '../../democraylist/democraylist.service';
 import {MenuItem} from 'primeng/api';
 import templateString from './playlist-cover.component.html'
 import stylesString from './playlist-cover.component.scss'
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-playlist-cover',
@@ -15,10 +16,15 @@ export class PlaylistCoverComponent implements OnInit {
   menuItems: MenuItem[];
 
   constructor(
-    private democraylistService: DemocraylistService
+    private democraylistService: DemocraylistService,
+    private router: Router
   ) { }
 
   ngOnInit(): void {
 
+  }
+
+  showPlaylist() {
+    this.router.navigate(['/playlists', this.playlist.id]);
   }
 }
